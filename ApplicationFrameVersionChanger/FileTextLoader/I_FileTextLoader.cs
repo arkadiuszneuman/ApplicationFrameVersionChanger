13c87794289b8ef61a4592904d92f2db2de3bbad
Arkadiusz Neuman <mrmoro@o2.pl>
2015-01-23 12:07:40 +0100

    Added file loader and saver

---
+++ ApplicationFrameVersionChanger/FileTextLoader/I_FileTextLoader.cs
@@ -0,0 +1,7 @@
+﻿namespace ApplicationFrameVersionChanger.FileTextLoader
+{
+    public interface I_FileTextLoader
+    {
+        string GetTextFromFile(string vrpFilePath);
+    }
+}
---
+++ ApplicationFrameVersionChanger/FileTextSaver/I_FileTextSaver.cs
@@ -0,0 +1,7 @@
+﻿namespace ApplicationFrameVersionChanger.FileTextSaver
+{
+    public interface I_FileTextSaver
+    {
+        void SaveText(string vrpFilePath, string vrpTextToSave);
+    }
+}
