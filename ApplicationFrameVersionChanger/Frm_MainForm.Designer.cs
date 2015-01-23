cf306186c7f19035abd40ec7dd469eb04fba9f21
Arkadiusz Neuman <mrmoro@o2.pl>
2015-01-23 12:09:46 +0100

    Checkout file in form

--- ApplicationFrameVersionChanger/Frm_MainForm.Designer.cs
+++ ApplicationFrameVersionChanger/Frm_MainForm.Designer.cs
@@ -33,8 +33,10 @@
             this.frtxtOutputDirectory = new DevExpress.XtraEditors.TextEdit();
             this.labelControl1 = new DevExpress.XtraEditors.LabelControl();
             this.btnChangeVersion = new DevExpress.XtraEditors.SimpleButton();
+            this.frbitTfs = new DevExpress.XtraEditors.CheckEdit();
             ((System.ComponentModel.ISupportInitialize)(this.frtxtSlnPath.Properties)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.frtxtOutputDirectory.Properties)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.frbitTfs.Properties)).BeginInit();
             this.SuspendLayout();
             // 
             // frtxtSlnPath
@@ -83,20 +85,31 @@
             this.btnChangeVersion.TabIndex = 6;
             this.btnChangeVersion.Text = "Zmień wersję";
             // 
+            // frbitTfs
+            // 
+            this.frbitTfs.EditValue = true;
+            this.frbitTfs.Location = new System.Drawing.Point(24, 77);
+            this.frbitTfs.Name = "frbitTfs";
+            this.frbitTfs.Properties.Caption = "Checkout plików w TFS";
+            this.frbitTfs.Size = new System.Drawing.Size(137, 19);
+            this.frbitTfs.TabIndex = 11;
+            // 
             // Frm_MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(686, 108);
+            this.Controls.Add(this.frbitTfs);
             this.Controls.Add(this.frtxtSlnPath);
             this.Controls.Add(this.labelControl2);
             this.Controls.Add(this.frtxtOutputDirectory);
             this.Controls.Add(this.labelControl1);
             this.Controls.Add(this.btnChangeVersion);
             this.Name = "Frm_MainForm";
-            this.Text = "Form1";
+            this.Text = "Zmiana wersji szkieletu";
             ((System.ComponentModel.ISupportInitialize)(this.frtxtSlnPath.Properties)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.frtxtOutputDirectory.Properties)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.frbitTfs.Properties)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -109,6 +122,7 @@
         private DevExpress.XtraEditors.TextEdit frtxtOutputDirectory;
         private DevExpress.XtraEditors.LabelControl labelControl1;
         private DevExpress.XtraEditors.SimpleButton btnChangeVersion;
+        private DevExpress.XtraEditors.CheckEdit frbitTfs;
 
     }
 }
